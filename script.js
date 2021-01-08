60467d2a7586f0ff745c58de794bb4b497752ab9
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2021-01-08 20:42:21 +0330

    Refactor:
    refactoring. temp 1

--- script.js
+++ script.js
@@ -1,33 +1,52 @@
-const usernameInput = document.getElementById('name');
-const emailInput = document.getElementById('emailInp');
-//const passwordInput = document.getElementById('passowrdInp');
-const userNameValidatorText = document.querySelector('.letter-count');
-const numberCheck = document.querySelector('.numberCheck');
-const validEmail = document.querySelector('.valid-email');
-//const passwordValidatorPath = /^[0-9a-zA-Z]+$/;
-const emailFormatpath = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-//check number
-/*passwordValidator = () => {
-	passwordInput.value.match(passwordValidatorPath)
-		? numberCheck.classList.replace('invalid', 'validIcon')
-		: numberCheck.classList.replace('validIcon', 'invalid');
-};*/
-//passwordInput.addEventListener('input', passwordValidator);
+// start validation
+const form = document.querySelector('#register-form');
+const userNameInput = document.querySelector('#username-input');
+const emailInput = document.querySelector('#email-input');
+const passwordInput = document.querySelector('#password-input');
+const password2Input = document.querySelector('#password2-input');
 
-//check email format
-checkEmailFormat = () => {
-	emailInput.value.match(emailFormatpath)
-		? validEmail.classList.replace('invalid', 'validIcon')
-		: validEmail.classList.replace('validIcon', 'invalid');
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	checkInputs();
+});
+
+const checkInputs = () => {
+	const usernameValue = userNameInput.value.trim();
+	const emailValue = emailInput.value.trim();
+	const passwordValue = passwordInput.value.trim();
+	const password2Value = password2Input.value.trim();
+
+	// checking username input
+	usernameValue == ''
+		? setError(userNameInput, 'نام کاربری را وارد کنید')
+		: setSuccess(userNameInput);
+
+	// checking email input
+	emailValue == ''
+		? setError(emailInput, 'ایمیل خود را وارد کنید')
+		: setSuccess(emailInput);
+
+	// checking passeord inputs
+	passwordValue == ''
+		? setError(passwordInput, 'رمز عبور را  وارد کنید')
+		: setSuccess(passwordInput);
+
+	if (password2Value == '') {
+		setError(password2Input, 'تکرار رمز عبور را وارد کنید');
+	} else if (passwordValue != password2Value) {
+		setError(password2Input, 'رمز عبور دوم را وارد کنید');
+	} else {
+		setSuccess(password2Input);
+	}
 };
 
-emailInput.addEventListener('input', checkEmailFormat);
-
-// check user name input
-letterCountCheck = () => {
-	usernameInput.value.length > 8
-		? userNameValidatorText.classList.replace('invalid', 'validIcon')
-		: userNameValidatorText.classList.replace('validIcon', 'invalid');
+const setError = (input, message) => {
+	input.placeholder = message;
+	input.classList.add('invalid');
 };
 
-usernameInput.addEventListener('input', letterCountCheck);
+const setSuccess = input => {
+	input.classList.remove('invalid');
+	input.classList.add('valid');
+};
+// end validation
