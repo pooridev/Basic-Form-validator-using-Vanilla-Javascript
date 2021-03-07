e9c6f09317560c92fc08ebde414d220c0de7aa25
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2021-03-07 15:40:41 +0330

    Refactor: better validation on inputs - temp 1

--- script.js
+++ script.js
@@ -15,33 +15,36 @@ const checkInputs = () => {
 	const emailValue = emailInput.value.trim();
 	const passwordValue = passwordInput.value.trim();
 	const password2Value = password2Input.value.trim();
-
+	// email pattern
+	const emailReg = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 	// checking username input
-	usernameValue == ''
+	usernameValue.length < 4
 		? setError(userNameInput, 'نام کاربری را وارد کنید')
 		: setSuccess(userNameInput);
 
 	// checking email input
-	emailValue == ''
-		? setError(emailInput, 'ایمیل خود را وارد کنید')
-		: setSuccess(emailInput);
+	emailValue.match(emailReg)
+		? setSuccess(emailInput)
+		: setError(emailInput, 'ایمیل خود را وارد کنید');
 
-	// checking passeord inputs
-	passwordValue == ''
-		? setError(passwordInput, 'رمز عبور را  وارد کنید')
+	// checking 1st password input
+	passwordValue.length < 8
+		? setError(passwordInput, 'حداقل باید 8 کاراکتر وارد کنید')
 		: setSuccess(passwordInput);
 
-	if (password2Value == '') {
-		setError(password2Input, 'تکرار رمز عبور را وارد کنید');
-	} else if (passwordValue != password2Value) {
-		setError(password2Input, 'رمز عبور دوم را وارد کنید');
-	} else {
-		setSuccess(password2Input);
-	}
+	// checking if two passwords are matches
+	password2Value.length < 8
+		? setError(password2Input, 'تکرار رمز عبور را وارد کنید')
+		: passwordValue != password2Value
+		? setError(password2Input, 'تکرار رمز عبور همخوانی ندارد')
+		: setSuccess(password2Input);
 };
 
 const setError = (input, message) => {
 	input.placeholder = message;
+	input.value = '';
+
+	input.classList.remove('valid');
 	input.classList.add('invalid');
 };
 
